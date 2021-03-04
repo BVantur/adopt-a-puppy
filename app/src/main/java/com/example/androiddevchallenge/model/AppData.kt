1da420cc4868d260ac3b7608346d86e76cef2e35
BVantur <blaz.vantur@gmail.com>
2021-03-04 11:39:35 +0100

    feat: added styling of texts and all screenshots and videos

--- app/src/main/java/com/example/androiddevchallenge/model/AppData.kt
+++ app/src/main/java/com/example/androiddevchallenge/model/AppData.kt
@@ -22,7 +22,7 @@ val dogs = arrayOf(
         "https://www.rd.com/wp-content/uploads/2019/01/shutterstock_385461856.jpg",
         "Spunky and sassy these cute little divas are viewed as the pampered breed, but it’s origin began far away from the penthouse set. Back in 19th century England, they were bred to chase rats. (They are a terrier after all, and all terriers were bred to chase and kill something.) You probably won’t find them chasing rats anymore, but they do love to play and give and receive attention and quite happy to sit pretty as a lapdog.",
         false,
-        "Short Description1",
+        "Bella is sometimes shy but she loves to play with her toys.",
         "Yorkshire terrier"
     ),
     Dog(
@@ -31,7 +31,7 @@ val dogs = arrayOf(
         "https://www.rd.com/wp-content/uploads/2021/01/GettyImages-131188311.jpg",
         "The Labrador retriever is a super friendly and easy-going breed and an ideal family dog, which is probably why it topped the American Kennel Club’s list of (AKC) most popular dog breeds for the last five years. These puppies, undeniably one of the cutest dog breeds, grow fast and reach adulthood by about six months and crave an active lifestyle to be happy and healthy. The only dilemma is which lab puppy is the cutest—black, yellow, or chocolate?",
         false,
-        "Short Description2",
+        "Luna likes to sleep.",
         "Labrador retriever"
     ),
     Dog(
@@ -40,7 +40,7 @@ val dogs = arrayOf(
         "https://www.rd.com/wp-content/uploads/2019/01/shutterstock_115329475.jpg",
         "Known for their keen intelligence, high energy, and loyalty, the German Shepherd is one of the most popular breeds of dog in the world. Despite their fearsome reputation, most owners find they are loyal, docile, obedient, and a loving family pet. It is because of their intelligence that German Shepherds are trained for military service, search and rescue, and police dogs. After World War I, returning soldiers spoke so highly of this breed that their popularity exploded in the United States. ",
         true,
-        "Short Description3",
+        "Speck wants to be a police dog.",
         "German shepherd"
     ),
     Dog(
@@ -49,7 +49,7 @@ val dogs = arrayOf(
         "https://www.rd.com/wp-content/uploads/2021/01/GettyImages-588935825.jpg",
         "If you relish the puppy stage and never want it to end, the golden retriever is one of the cutest dog breeds—and slowest breeds to mature when it comes to silly playfulness traits. It loves being social and becoming a real member of the family, so it will be much happier with you and not alone in the yard for hours on end.",
         false,
-        "Short Description4",
+        "Daisy likes flowers.",
         "Golden retriever"
     ),
     Dog(
@@ -58,7 +58,7 @@ val dogs = arrayOf(
         "https://www.rd.com/wp-content/uploads/2019/01/shutterstock_589686617.jpg",
         "That smooshed in face is irresistibly cute, but it can cause some breathing issues. The Frenchie is a brachycephalic breed, which means its shorter snouts make breathing a little more laborious, especially in hot weather or if your Frenchie takes a dip in the lake. But the Frenchie’s natural inclination to be a champion couch potato suits it’s breathing condition so it won’t have issues there. When they do want to cool off and relax they lay with their legs splayed out behind them, earning another nickname, “Frog Dog.”",
         true,
-        "Short Description5",
+        "Hooch's hobby is chewing shoes.",
         "French bulldog"
     ),
     Dog(
@@ -67,7 +67,7 @@ val dogs = arrayOf(
         "https://www.rd.com/wp-content/uploads/2019/01/shutterstock_123087826-e1548785863702.jpg",
         "It’s probably one of the cutest dog breeds because the beagle keeps its adorable puppy face for most of its life—until those gray hairs eventually creep in around its face. These pups are motivated by scent and an ever-present curiosity that is driven by their keen sense of smell and the desire to chase down any scent that intrigues them. Roaming and exploring are genetic so they feel their best with a family that loves an active lifestyle outdoors.",
         true,
-        "Short Description6",
+        "Chopper likes to play indoors.",
         "Beagle"
     ),
     Dog(
@@ -76,7 +76,7 @@ val dogs = arrayOf(
         "https://www.rd.com/wp-content/uploads/2019/01/shutterstock_673465372.jpg",
         "The fluffy and “foo-foo” appearance of poodles hardly conjures up what poodles were originally bred to do—retrieve prey from the water but all that fur (which is actually hair) protects joints from the cold water. Their hair doesn’t shed too much or produce too much dander so they’re often great companions for allergy sufferers who want a snuggler, which suits the poodle just fine because they assume they are a member of your family and prefer quality time with you. ",
         false,
-        "Short Description7",
+        "Lola likes to go to hairdresser.",
         "Poodles"
     ),
     Dog(
@@ -85,7 +85,7 @@ val dogs = arrayOf(
         "https://www.rd.com/wp-content/uploads/2019/01/shutterstock_493937641.jpg",
         "Oh my goodness… the chunky cuteness of a “Rottie” puppy face is undeniable. Rottie puppies are a solid block of burning love. Their stocky and muscular body tips the scales from 95 to 135 pounds, males on the heavier end. Male Rottweilers are calm and always on duty, watching for threats to their home and human family but females are a little more chill and more affectionate. They require firm and consistent training from the start so if you don’t have time to devote to this, the Rottie isn’t the dog for you.",
         true,
-        "Short Description8",
+        "Bolt likes to run fast.",
         "Rottweiler"
     ),
 )
--- app/src/main/java/com/example/androiddevchallenge/ui/theme/Color.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/theme/Color.kt
@@ -17,7 +17,7 @@ package com.example.androiddevchallenge.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val purple200 = Color(0xFFBB86FC)
-val purple500 = Color(0xFF6200EE)
-val purple700 = Color(0xFF3700B3)
-val teal200 = Color(0xFF03DAC5)
+val brown200 = Color(0xFFA98274)
+val brown500 = Color(0xFF795548)
+val brown700 = Color(0xFF4B2C20)
+val teal200 = Color(0xFFFFC246)
